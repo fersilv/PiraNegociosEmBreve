14f91c495ae1f72cd9861eaec532fe73777df4a2
fersilv <167830576+fersilv@users.noreply.github.com>
2026-08-22 12:40:22 -0300

    feat: register push installations repository

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notification } from './entities/notification.entity';
+import { PushInstallation } from './entities/push-installation.entity';
 import { User } from '../users/entities/user.entity';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification, User])],
+  imports: [TypeOrmModule.forFeature([Notification, PushInstallation, User])],
   providers: [NotificationsService],
   controllers: [NotificationsController],
   exports: [NotificationsService],
