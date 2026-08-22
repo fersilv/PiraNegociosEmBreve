42d4f8d78fa261f9f2fbba35c87a445ffa2d7d38
fersilv <167830576+fersilv@users.noreply.github.com>
2026-08-21 23:47:48 -0300

    feat: connect application events to notifications

--- backend/src/applications/applications.module.ts
+++ backend/src/applications/applications.module.ts
@@ -5,9 +5,10 @@ import { Job } from '../jobs/entities/job.entity';
 import { User } from '../users/entities/user.entity';
 import { ApplicationsService } from './applications.service';
 import { ApplicationsController } from './applications.controller';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Application, Job, User])],
+  imports: [TypeOrmModule.forFeature([Application, Job, User]), NotificationsModule],
   providers: [ApplicationsService],
   controllers: [ApplicationsController],
 })
