6277290f3c39a9b5475bd7e0257a572b982f2b1d
fersilv <167830576+fersilv@users.noreply.github.com>
2026-08-22 20:21:57 -0300

    feat: export billing support service

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -4,11 +4,12 @@ import { AdminGuard } from '../admin/admin.guard';
 import { User } from '../users/entities/user.entity';
 import { PaymentsController, AdminPaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { BillingSupportService } from './billing-support.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [PaymentsController, AdminPaymentsController],
-  providers: [PaymentsService, AdminGuard],
-  exports: [PaymentsService],
+  providers: [PaymentsService, BillingSupportService, AdminGuard],
+  exports: [PaymentsService, BillingSupportService],
 })
 export class PaymentsModule {}
