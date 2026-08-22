e94fb326441ceb6d3b6548f01659e3e30249816b
fersilv <167830576+fersilv@users.noreply.github.com>
2026-08-22 20:28:57 -0300

    feat: register paid product duration service

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -5,11 +5,12 @@ import { User } from '../users/entities/user.entity';
 import { PaymentsController, AdminPaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { BillingSupportService } from './billing-support.service';
+import { ProductDurationService } from './product-duration.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [PaymentsController, AdminPaymentsController],
-  providers: [PaymentsService, BillingSupportService, AdminGuard],
-  exports: [PaymentsService, BillingSupportService],
+  providers: [PaymentsService, BillingSupportService, ProductDurationService, AdminGuard],
+  exports: [PaymentsService, BillingSupportService, ProductDurationService],
 })
 export class PaymentsModule {}
