808518285617ea2f540ef90e801de71d446bbcc7
fersilv <167830576+fersilv@users.noreply.github.com>
2026-08-22 20:09:36 -0300

    feat: register job match module

---
+++ backend/src/job-match/job-match.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminModule } from '../admin/admin.module';
+import { AdminGuard } from '../admin/admin.guard';
+import { PaymentsModule } from '../payments/payments.module';
+import { Job } from '../jobs/entities/job.entity';
+import { User } from '../users/entities/user.entity';
+import { JobMatchAiService } from './job-match-ai.service';
+import { AdminJobMatchController, JobMatchController } from './job-match.controller';
+import { JobMatchService } from './job-match.service';
+
+@Module({
+  imports: [AdminModule, PaymentsModule, TypeOrmModule.forFeature([Job, User])],
+  controllers: [JobMatchController, AdminJobMatchController],
+  providers: [AdminGuard, JobMatchAiService, JobMatchService],
+  exports: [JobMatchService],
+})
+export class JobMatchModule {}
