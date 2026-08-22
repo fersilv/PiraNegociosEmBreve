10f960eaafc3dd60fee6a2637507e0a1f0245172
fersilv <167830576+fersilv@users.noreply.github.com>
2026-08-22 18:43:04 -0300

    fix: route resume PDF button through isolated print renderer

--- index.tsx
+++ index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { registerPwaServiceWorker } from './lib/pwa';
+import { initResumePrint } from './lib/resumePrint';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
+
+initResumePrint();
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
