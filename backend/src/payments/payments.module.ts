2fee320c07e26df9a55203a2fb0ecf2ee71ea743
fersilv <167830576+fersilv@users.noreply.github.com>
2026-08-23 09:31:23 -0300

    feat: register Efí Pix payment services

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminGuard } from '../admin/admin.guard';
 import { User } from '../users/entities/user.entity';
-import { PaymentsController, AdminPaymentsController } from './payments.controller';
+import {
+  PaymentsController,
+  AdminPaymentsController,
+  EfiPaymentsWebhookController,
+} from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { BillingSupportService } from './billing-support.service';
 import { ProductDurationService } from './product-duration.service';
+import { EfiPixService } from './efi-pix.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [PaymentsController, AdminPaymentsController],
-  providers: [PaymentsService, BillingSupportService, ProductDurationService, AdminGuard],
-  exports: [PaymentsService, BillingSupportService, ProductDurationService],
+  controllers: [PaymentsController, EfiPaymentsWebhookController, AdminPaymentsController],
+  providers: [PaymentsService, BillingSupportService, ProductDurationService, EfiPixService, AdminGuard],
+  exports: [PaymentsService, BillingSupportService, ProductDurationService, EfiPixService],
 })
 export class PaymentsModule {}
