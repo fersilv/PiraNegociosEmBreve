2a6e3ddcd45dc7656ac404c4cd43ffb6077838ea
fersilv <167830576+fersilv@users.noreply.github.com>
2026-08-31 00:31:23 -0300

    feat: expoe manutencao das regras de entrega

--- pages/AdminDeliveryPartnersPage.tsx
+++ pages/AdminDeliveryPartnersPage.tsx
@@ -1,1 +1,12 @@
-export { default } from './AdminDeliveryPartnersPageV3';
+import React from 'react';
+import { AdminDeliveryRuleManager } from '../components/AdminDeliveryRuleManager';
+import AdminDeliveryPartnersPageV3 from './AdminDeliveryPartnersPageV3';
+
+export default function AdminDeliveryPartnersPage() {
+  return (
+    <>
+      <AdminDeliveryPartnersPageV3 />
+      <AdminDeliveryRuleManager />
+    </>
+  );
+}
